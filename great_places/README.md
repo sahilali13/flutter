8bbd8a4ee31eff96d762c0d1cd6af96c93112781
sahilali13 <ali.sahil.a743@gmail.com>
2021-10-18 22:25:06 +0530

    commit #312

--- great_places/README.md
+++ great_places/README.md
@@ -5,4 +5,7 @@ Flutter & Dart - The Complete Guide [2021 Edition]
 Section 13
 
 
-Replace API_KEY
+Replace API_KEY in 
+    helpers/location_helper.dart
+    great_places/ios/Runner/AppDelegate.swift
+    great_places/android/app/src/main/AndroidManifest.xml
--- great_places/ios/Runner/AppDelegate.swift
+++ great_places/ios/Runner/AppDelegate.swift
@@ -9,7 +9,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyAGHhQZSaEruOQuUlF-QP_o0dCQmNO4yTU")
+    GMSServices.provideAPIKey("[API_KEY]")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
